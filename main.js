95b08bd319312570ce84f8e0d1bf35fa340ba3f5
b-price <bprice12@students.towson.edu>
2024-03-04 16:49:34 -0500

    changed import in main.js to cdn

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.162.0/build/three.module.js';
 // import { Color } from 'three';
 
 // import Stats from 'three/addons/libs/stats.module.js';
